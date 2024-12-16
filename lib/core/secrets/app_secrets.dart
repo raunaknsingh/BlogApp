cd233eb60d45acc213a350f975563695f6bcbbdb
raunak <raunak.singh@getir.com>
2024-12-16 21:31:25 +0530

    Add Supabase dependency into the project

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://blrwqaktqullsklmyooy.supabase.co';
+  static const supabaseAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscndxYWt0cXVsbHNrbG15b295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzNjI4MzcsImV4cCI6MjA0OTkzODgzN30.dzJ7QmMI_7BM3QyyYPN2VB2M1abDEOV7rlL2iG1QnWw';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:blog_app/core/app_theme.dart';
+import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/presentation/screens/signin_page.dart';
 import 'package:blog_app/features/auth/presentation/screens/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized()
+  final supabase = await Supabase.initialize(
+      url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseAnon);
   runApp(const MyApp());
 }
 
@@ -20,4 +25,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
